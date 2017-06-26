29b659be87773ac4f34bb4b7c7adb4477296e69a
Yancheng Chen <yc2739@nyu.edu>
2017-06-26 23:08:55 +0800

    Add files via upload

---
+++ 106-1.py
@@ -0,0 +1,13 @@
+Python 2.7.12 (v2.7.12:d33e0cf91556, Jun 27 2016, 15:24:40) [MSC v.1500 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> #coding:utf-8
+>>> def add_function(a,b)
+SyntaxError: invalid syntax
+>>> def add_function(a,b):
+	c=a+b
+	printc
+
+	
+>>> if _name_=="_main_":
+	add_function(2,3)
+	
