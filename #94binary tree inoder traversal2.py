eda93747ab7eaff7a7494790124a935db76bd71c
Yancheng Chen <yc2739@nyu.edu>
2017-07-12 16:16:03 +0800

    Add files via upload

---
+++ #94binary tree inoder traversal2.py
@@ -0,0 +1,23 @@
+#94. Binary Tree Inorder Traversal / 39ms /62.36%
+# Definition for a  binary tree node
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    # @param root, a tree node
+    # @return a list of integers
+    def inorderTraversal(self, root):
+        p = root
+        stack = []
+        ans = []
+        while p or stack:
+            while p:
+                stack.append(p)
+                p = p.left
+            p = stack.pop()
+            ans.append(p.val)
+            p = p.right
+        return ans
