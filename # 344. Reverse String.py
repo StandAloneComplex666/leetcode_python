ce8af7b332f95301dd723c8e733e66b3ea573c2a
Yancheng Chen <yc2739@nyu.edu>
2017-07-27 15:29:01 +0800

    Delete # 344. Reverse String.py

--- # 344. Reverse String.py
+++
@@ -1,9 +0,0 @@
-#344. Reverse String
-#35ms fastest
-class Solution(object):
-    def reverseString(self, s):
-        """
-        :type s: str
-        :rtype: str
-        """
-        return s[::-1]
