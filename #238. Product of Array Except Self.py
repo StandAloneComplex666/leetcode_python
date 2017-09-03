54f3ecfb28f0b2965faad9b973f059b627ce09b5
Yancheng Chen <yc2739@nyu.edu>
2017-09-03 14:31:43 -0400

    Add files via upload

---
+++ #238. Product of Array Except Self.py
@@ -0,0 +1,16 @@
+#238. Product of Array Except Self.py
+class Solution:
+    # @param {integer[]} nums
+    # @return {integer[]}
+    def productExceptSelf(self, nums):
+        size = len(nums)
+        output = [1] * size
+        left = 1
+        for x in range(size - 1):
+            left *= nums[x]
+            output[x + 1] *= left
+        right = 1
+        for x in range(size - 1, 0, -1):
+            right *= nums[x]
+            output[x - 1] *= right
+        return output
