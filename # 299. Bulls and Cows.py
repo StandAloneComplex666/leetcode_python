4f23859da43ea09fc45b826bf9c0f2d44ab57fef
DESKTOP-O8OGVNP\hp <yc2739@nyu.edu>
2018-02-22 16:33:45 -0500

    no message

--- # 299. Bulls and Cows.py
+++ # 299. Bulls and Cows.py
@@ -31,24 +31,24 @@ def getHint(self, secret, guess):
 :type guess: str
 :rtype: str
 """
-secret_map, guess_map = {}, {}
-bull_count, cow_count = 0, 0
-for i in range(len(secret)):
-    if secret[i] == guess[i]:
-        bull_count += 1
-    else:
-        if secret[i] in secret_map:
-            secret_map[secret[i]] += 1
+    secret_map, guess_map = {}, {}
+    bull_count, cow_count = 0, 0
+    for i in range(len(secret)):
+        if secret[i] == guess[i]:
+            bull_count += 1
         else:
-            secret_map[secret[i]] = 1
-        if guess[i] in guess_map:
-            guess_map[guess[i]] += 1
-        else:
-            guess_map[guess[i]] = 1
-for i in guess_map:
-    if i in secret_map:
-        cow_count += min(secret_map[i], guess_map[i])
-return '%sA%sB' % (bull_count, cow_count)
+            if secret[i] in secret_map:
+                secret_map[secret[i]] += 1
+            else:
+                secret_map[secret[i]] = 1
+            if guess[i] in guess_map:
+                guess_map[guess[i]] += 1
+            else:
+                guess_map[guess[i]] = 1
+    for i in guess_map:
+        if i in secret_map:
+            cow_count += min(secret_map[i], guess_map[i])
+    return '%sA%sB' % (bull_count, cow_count)
 
 def getHint(self, secret, guess):
 """
