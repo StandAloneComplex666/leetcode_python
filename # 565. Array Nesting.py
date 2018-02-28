a5bfd0472782c131c7d7c6cdc574839dd107c090
DESKTOP-O8OGVNP\hp <yc2739@nyu.edu>
2018-02-27 21:23:09 -0500

    no message

---
+++ # 565. Array Nesting.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def arrayNesting(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        ans, step, n = 0, 0, len(nums)
+        seen = [False] * n
+        for i in range(n):
+            while not seen[i]:
+                seen[i] = True
+                i, step = nums[i], step + 1
+            ans = max(ans, step)
+            step = 0
+        return ans
