e1ff714499d73331244dff03e6ba7037dffd1253
DESKTOP-O8OGVNP\hp <yc2739@nyu.edu>
2018-03-29 15:49:37 -0400

    no message

---
+++ # 554. Brick Wall.py
@@ -0,0 +1,43 @@
+# a very naive way which will cause memory limit error
+class Solution(object):
+    def leastBricks(self, wall):
+        """
+        :type wall: List[List[int]]
+        :rtype: int
+        """
+        counts = [0 for i in range(len(wall))]
+        indexs = [0 for i in range(len(wall))]
+        min_bricks = 10001
+        width = sum(wall[0])
+        print(width)
+        for i in range(width):
+            temp = 0
+            for j in range(len(wall)):
+                if counts[j] == wall[j][indexs[j]]:
+                    indexs[j] += 1
+                    counts[j] = 0
+                else:
+                    temp += 1
+                    counts[j] += 1
+            min_bricks = min(temp, min_bricks)
+        return min_bricks
+# beat 100% 一雪前耻！
+class Solution(object):
+    def leastBricks(self, wall):
+        """
+        :type wall: List[List[int]]
+        :rtype: int
+        """
+        space_dict = {}
+        for row in wall:
+            len_row = 0
+            for brick in row[:-1]:
+                len_row += brick
+                if len_row not in space_dict:
+                    space_dict[len_row] = 1
+                else:
+                    space_dict[len_row] += 1
+        if space_dict:
+            return len(wall) - max(space_dict.values())
+        else:
+            return len(wall)
