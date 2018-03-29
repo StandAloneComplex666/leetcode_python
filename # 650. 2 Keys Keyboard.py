9008c1847a20688bffb3f05ab526fdcbb8039f61
DESKTOP-O8OGVNP\hp <yc2739@nyu.edu>
2018-03-28 20:19:51 -0400

    no message

--- # 650. 2 Keys Keyboard.py
+++ # 650. 2 Keys Keyboard.py
@@ -1,18 +1,13 @@
-# 650. 2 Keys Keyboard
 class Solution(object):
     def minSteps(self, n):
         """
         :type n: int
         :rtype: int
         """
-        def factors(n):
-            d = 2
-            while d * d <= n:
-                while n % d == 0:
-                    n /= d
-                    yield d
-                d += 1
-            if n > 1:
-                yield n
-
-        return sum(factors(n))
+        if n == 1:
+            return 0
+        for i in range(2,n+1):
+            if (n%i == 0):
+                #print(i)
+                return i+self.minSteps(n/i)
+        return n
