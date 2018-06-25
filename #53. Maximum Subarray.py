c9d7ce44b0056f8d210b519571bfe77f71f703d2
DESKTOP-O8OGVNP\hp <yc2739@nyu.edu>
2018-06-25 15:31:35 -0400

    no message

--- #53. Maximum Subarray.py
+++ #53. Maximum Subarray.py
@@ -10,3 +10,15 @@ class Solution(object):
             currentSum = max(nums[i],nums[i]+currentSum)
             maxSum = max(currentSum,maxSum)
         return maxSum
+
+# another kind of dp method
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        for i in range(1, len(nums)):
+                if nums[i-1] > 0:
+                    nums[i] += nums[i-1]
+        return max(nums)
