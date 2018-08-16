9de07660bd803fb209009b5e39facb696f144675
DESKTOP-O8OGVNP\hp <yc2739@nyu.edu>
2018-08-16 14:27:50 -0400

    no message

--- #117. Populating Next Right Pointers in Each Node.py
+++ #117. Populating Next Right Pointers in Each Node.py
@@ -24,3 +24,33 @@ class Solution:
                     if nextNode == None: nextNode = q
                 p = p.next
             self.connect(nextNode)
+
+
+# a new version which is implemented by iteration, not recursion
+# Definition for a  binary tree node
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+#         self.next = None
+
+class Solution:
+    # @param root, a tree node
+    # @return nothing
+    def connect(self, root):
+        curr = root
+        while curr:
+            firstNext = None
+            prev = None
+            while curr:
+                if not firstNext:
+                    firstNext = curr.left if curr.left else curr.right
+                if curr.left:
+                    if prev:prev.next = curr.left
+                    prev = curr.left
+                if curr.right:
+                    if prev:prev.next = curr.right
+                    prev = curr.right
+                curr = curr.next
+            curr = firstNext
