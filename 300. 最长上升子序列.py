14df68c6ea839e73d6ee1ec9998d2c7f01d2b5c6
Chen Yancheng <Yancheng.chen@jiliguala.com>
2020-10-04 16:42:42 +0800

    no message

---
+++ 300. 最长上升子序列.py
@@ -0,0 +1,31 @@
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        dp = []
+        for i in range(len(nums)):
+            dp.append(1)
+            for j in range(i):
+                if nums[i] > nums[j]:
+                    dp[i] = max(dp[i], dp[j] + 1)
+        return max(dp)
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        d = []
+        for n in nums:
+            if not d or n > d[-1]:
+                d.append(n)
+            else:
+                l, r = 0, len(d) - 1
+                loc = r
+                while l <= r:
+                    mid = (l + r) // 2
+                    if d[mid] >= n:
+                        loc = mid
+                        r = mid - 1
+                    else:
+                        l = mid + 1
+                d[loc] = n
+        return len(d)
